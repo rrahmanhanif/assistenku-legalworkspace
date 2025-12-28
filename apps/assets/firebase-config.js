81bba86c75fa27a17e66f53f0af346cb8f95c94f
rrahmanhanif <rrahmanhanif@gmail.com>
2025-12-28 14:28:07 +0700

    Create firebase-config.js

---
+++ apps/assets/firebase-config.js
@@ -0,0 +1,13 @@
+// Konfigurasi Firebase Produksi
+// Digunakan oleh seluruh aplikasi (Login, Client, Mitra, Admin)
+
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBSL87qkuwSQU8aXvLuu24nV7jUoX2mOSA",
+  authDomain: "assistenku-8ef85.firebaseapp.com",
+  databaseURL: "https://assistenku-8ef85-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "assistenku-8ef85",
+  storageBucket: "assistenku-8ef85.firebasestorage.app",
+  messagingSenderId: "320243806907",
+  appId: "1:320243806907:web:a78bd29ee497aa04ee2f9e",
+  measurementId: "G-CB25PSK4J4"
+};
