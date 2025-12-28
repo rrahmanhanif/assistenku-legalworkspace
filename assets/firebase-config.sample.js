b6b50d5f2b87c250f4a7db76fcef4b730b78f9c1
rrahmanhanif <rrahmanhanif@gmail.com>
2025-12-28 18:17:41 +0700

    Update firebase-config.sample.js

--- assets/firebase-config.sample.js
+++ assets/firebase-config.sample.js
@@ -1,14 +1,3 @@
-/**
- * Firebase Configuration (Sample)
- *
- * Salin file ini menjadi:
- *   assets/firebase-config.js
- *
- * Lalu isi nilainya dengan konfigurasi Firebase project Anda.
- * Konfigurasi dapat ditemukan di:
- * Firebase Console → Project Settings → General → SDK setup and configuration
- */
-
 window.FIREBASE_CONFIG = {
   apiKey: "ISI_API_KEY_ANDA",
   authDomain: "project-id.firebaseapp.com",
