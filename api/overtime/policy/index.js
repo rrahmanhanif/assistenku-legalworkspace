f19c3725d9b629cc0c8d4578b9db846ffca9ac3a
rrahmanhanif <rrahmanhanif@gmail.com>
2025-12-31 19:05:37 +0700

    Create index.js

---
+++ api/overtime/policy/index.js
@@ -0,0 +1,15 @@
+import { json } from "../../_lib/http.js";
+import { requireUser } from "../../_lib/auth.js";
+import { sbSelect } from "../../_lib/supabase-rest.js";
+
+export default async function handler(req, res) {
+  if (req.method !== "GET") return json(res, 405, { message: "Method Not Allowed" });
+
+  try {
+    await requireUser(req, { expectedRole: "ADMIN" });
+    const data = await sbSelect("overtime_policy", {});
+    return json(res, 200, { policies: data });
+  } catch (err) {
+    return json(res, err.statusCode || 500, { message: err.message });
+  }
+}
