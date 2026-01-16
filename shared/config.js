7654574736e06ec433e580162de3b50a2c71fc17
rrahmanhanif <rrahmanhanif@gmail.com>
2026-01-16 23:06:27 +0700

    Update config.js

--- shared/config.js
+++ shared/config.js
@@ -1,1 +1,14 @@
 window.__API_BASE__ = "https://api.assistenku.com";
+
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBSL87qkuwSQU8aXvLuu24nV7jUoX2mOSA",
+  authDomain: "assistenku-8ef85.firebaseapp.com",
+  databaseURL: "https://assistenku-8ef85-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "assistenku-8ef85",
+  storageBucket: "assistenku-8ef85.firebasestorage.app",
+  messagingSenderId: "320243806907",
+  appId: "1:320243806907:web:a78bd29ee497aa04ee2f9e",
+  measurementId: "G-CB25PSK4J4"
+};
+
+window.LEGALWORKSPACE_BASE_URL = "https://legalworkspace.assistenku.com";
