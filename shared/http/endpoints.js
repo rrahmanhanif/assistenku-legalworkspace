a5ee1b695125efb6989336180637180a91345683
rrahmanhanif <rrahmanhanif@gmail.com>
2026-01-23 23:33:54 +0700

    Create endpoints.js

---
+++ shared/http/endpoints.js
@@ -0,0 +1,10 @@
+// shared/http/endpoints.js
+export const endpoints = {
+  auth: {
+    whoami: "/api/auth/whoami",
+    requestLink: "/api/auth/request-link"
+  },
+  legal: {
+    docsRegister: "/api/legal/docs/register"
+  }
+};
